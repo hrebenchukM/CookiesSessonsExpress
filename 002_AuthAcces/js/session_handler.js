66b31ab4270e663c879f5eec80dfb7be2b87fd9f
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-10-21 03:32:02 +0300

    //

--- 002_AuthAcces/js/session_handler.js
+++ 002_AuthAcces/js/session_handler.js
@@ -1,24 +1,28 @@
 ﻿var cookieParser = require('cookie-parser');
 var session = require('express-session');
-
 // подключение модуля connect-mssql
 var MSSQLStore = require('connect-mssql')(session);
 var mssql = require('mssql'); 
+// параметры соединения с бд
+var config = {
+	user: 'admin',           // пользователь базы данных
+	password: '12345',          // пароль пользователя 
+	server: 'LENOVO\\SQLEXPRESS',       // хост
+	database: 'ATB',          // имя бд
+	port: 1433,             // порт, на котором запущен sql server
+	  options: {
+		  encrypt: true,  // Использование SSL/TLS
+		  trustServerCertificate: true // Отключение проверки самоподписанного сертификата
+	  },
+ }
+
 
 module.exports = {
-    createStore: function () {
-        var config = {
-            user: 'admin',           // пользователь базы данных
-            password: '12345',          // пароль пользователя 
-            server: 'LENOVO\\SQLEXPRESS',       // хост
-            database: 'testdb',          // имя бд
-            port: 1433,             // порт, на котором запущен sql server
-              options: {
-                  encrypt: true,  // Использование SSL/TLS
-                  trustServerCertificate: true // Отключение проверки самоподписанного сертификата
-              },
-          }
+    getConfig: function () {
+        return config;
+    },
 
+    createStore: function () {
         return new MSSQLStore(config);
     }
 }
--- 002_AuthAcces/main.js
+++ 002_AuthAcces/main.js
@@ -5,30 +5,36 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
+var mssql = require('mssql'); 
 
 var jsonParser = bodyParser.json();
 app.use(jsonParser);
 
 var port = 8080;
 
-// зарегистрированные пользователи, которые могут быть авторизованы
-var users = [
-    { username: 'admin', password: '12345' },
-    { username: 'foo', password: 'bar' },
-    { username: 'user', password: 'test' }
-]
+
 
 // создание хранилища для сессий 
 var sessionHandler = require('./js/session_handler');
 var store = sessionHandler.createStore();
+var config = sessionHandler.getConfig(); 
+
+
 
 // создание сессии 
 app.use(cookieParser());
 app.use(session({
     saveUninitialized: true,
-    secret: 'supersecret'
+    secret: 'supersecret',
+    resave: false
 }));
 
+// зарегистрированные пользователи, которые могут быть авторизованы
+// var users = [
+//     { username: 'admin', password: '12345' },
+//     { username: 'foo', password: 'bar' },
+//     { username: 'user', password: 'test' }
+// ]
 app.set('views', path.join(__dirname, 'pages'));
 app.set('view engine', 'ejs');
 
@@ -36,29 +42,92 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/login', function (req, res) {
-    var foundUser;
-    // поиск пользователя в массиве users 
-    for (var i = 0; i < users.length; i++) {
-        var u = users[i];
-        if (u.username == req.body.username && u.password == req.body.password) {
-            foundUser = u.username
-        };
-    };
 
-    if (foundUser !== undefined) {
-        req.session.username = req.body.username;
-        console.log("Login succeeded: ", req.session.username);
-        res.send('Login successful: ' + 'sessionID: ' + req.session.id + '; user: ' + req.session.username);
-    } else {
-        console.log("Login failed: ", req.body.username)
-        res.status(401).send('Login error');
-    }
+app.post('/login',function (req, res) {
+
+	console.log(req.body);
+
+    let Login = req.body.Login;
+    let Password = req.body.Password;
+  
+var connection = new mssql.ConnectionPool(config);
+
+
+
+	connection.connect(config,function (err) {
+        if (err) {
+            console.log("Error connecting to db:", err);
+        } else {
+            console.log("Connecting to db Ok!");
+        }
+		// транзакция - безопасная операция над бд с возможностью отката изменений в случае ошибки при выполнении запроса  
+		var transaction = new mssql.Transaction(connection);
+
+
+		transaction.begin(function (err) {
+			var request = new mssql.Request(transaction);
+			request.input('Login', mssql.NVarChar(50), Login);
+			request.input('Password', mssql.NVarChar(50), Password);
+
+
+			request.query(`
+                SELECT * 
+                FROM Admins 
+                WHERE Login = @Login AND Password = @Password
+            `, function (err, data) {
+
+				if (err) {
+					console.log(err);
+					transaction.rollback(function (err) {
+						console.log('rollback successful');
+						res.send('transaction rollback successful');
+					});
+				} 
+				else {
+					transaction.commit(function (err) {
+							console.log('data commit success');
+
+
+							var allItems = data.recordset;
+			
+
+							if (allItems.length > 0) {
+                             req.session.Login = Login;
+						     console.log("Login succeeded: ", req.session.Login);
+                             res.send('Login successful: ' + 'sessionID: ' + req.session.id + '; user: ' + req.session.Login);
+   
+						}else {
+							       console.log("Login failed: ", req.body.Login)
+                                   res.status(401).send('Login error');
+						}
+                    });
+                }
+            });
+        });
+});
+
+    // var foundUser;
+    // // поиск пользователя в массиве users 
+    // for (var i = 0; i < users.length; i++) {
+    //     var u = users[i];
+    //     if (u.username == req.body.username && u.password == req.body.password) {
+    //         foundUser = u.username
+    //     };
+    // };
+
+    // if (foundUser !== undefined) {
+    //     req.session.username = req.body.username;
+    //     console.log("Login succeeded: ", req.session.username);
+    //     res.send('Login successful: ' + 'sessionID: ' + req.session.id + '; user: ' + req.session.username);
+    // } else {
+    //     console.log("Login failed: ", req.body.username)
+    //     res.status(401).send('Login error');
+    // }
 
 });
 
 app.get('/logout', function (req, res) {
-    req.session.username = '';
+    req.session.Login = '';
     console.log('logged out');
     res.send('logged out!');
 });
@@ -66,8 +135,8 @@ app.get('/logout', function (req, res) {
 // ограничение доступа к контенту на основе авторизации 
 app.get('/admin', function (req, res) {
     // страница доступна только для админа 
-    if (req.session.username == 'admin') {
-        console.log(req.session.username + ' requested admin page');
+    if (req.session.Login == 'admin') {
+        console.log(req.session.Login + ' requested admin page');
         res.render('admin_page');
     } else {
         res.status(403).send('Access Denied!');
@@ -77,8 +146,8 @@ app.get('/admin', function (req, res) {
 
 app.get('/user', function (req, res) {
     // страница доступна для любого залогиненного пользователя 
-    if (req.session.username.length > 0) {
-        console.log(req.session.username + ' requested user page');
+    if (req.session.Login.length > 0) {
+        console.log(req.session.Login + ' requested user page');
         res.render('user_page');
     } else {
         res.status(403).send('Access Denied!');
